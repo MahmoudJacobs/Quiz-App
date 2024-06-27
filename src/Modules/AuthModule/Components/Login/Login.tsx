27e68f5f51356bed613b5ba68767736f38a9e6cb
Omar Bazeed <114960595+OmarBazeed@users.noreply.github.com>
2024-06-27 17:05:05 +0300

    start to use tailwind.css

--- src/Modules/AuthModule/Components/Login/Login.tsx
+++ src/Modules/AuthModule/Components/Login/Login.tsx
@@ -1,7 +1,7 @@
 export default function login() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-center text-amber-800 hover:bg-slate-400">
+    <div className="h-screen bg-slate-500 w-full">
+      <h1 className="text-2xl font-bold text-center text-amber-800 hover:bg-cyan-950 h-screen">
         login
       </h1>
     </div>
